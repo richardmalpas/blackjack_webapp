4e0e4f0040ea5c5d3ce5279fe4bc4a1616462e79
richardmalpas <34751386+richardmalpas@users.noreply.github.com>
2021-09-03 09:47:46 +0100

    modified setup_script.sh and app.py

--- app.py
+++ app.py
@@ -146,5 +146,5 @@ def concatinate_cards(player_hand_list):
 
 
 if __name__ == "__main__":
-    app.run(debug=False)
-    #app.run(host="0.0.0.0", port="80", debug=False)
+    #app.run(debug=False)
+    app.run(host="0.0.0.0", port="80", debug=False)
--- setup_script.sh
+++ setup_script.sh
@@ -15,6 +15,5 @@ envsubst < setup_nginx.conf > nginx.conf
 sudo apt install nginx -y
 sudo apt install supervisor -y
 sudo rm /etc/nginx/sites-enabled/default
-cd ~/blackjack_webapp/
 sudo cp ~/blackjack_webapp/nginx.conf /etc/nginx/sites-enabled/blackjack_webapp
 gunicorn -w 3 app:app
