f11cd587f0f48c9e3ead3f46b81e413078f64171
richardmalpas <34751386+richardmalpas@users.noreply.github.com>
2021-09-19 21:43:04 +0100

    updates to uninstall_script.sh

--- uninstall_script.sh
+++ uninstall_script.sh
@@ -4,6 +4,6 @@ sudo pip3 uninstall -r requirements.txt -y
 sudo apt remove nginx -y
 sudo apt remove snapd -y
 sudo apt remove supervisor -y
-deactivate
-sudo rm -r blackjack_webapp
+bash deactivate
+sudo rm -r ./blackjack_webapp
 echo "Uninstallation completed successfully"
