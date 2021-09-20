14dd6e2560a435caf806cd04bf31ce596625a23f
richardmalpas <34751386+richardmalpas@users.noreply.github.com>
2021-09-20 15:26:53 +0100

    updates to uninstall_script.sh

--- uninstall_script.sh
+++ uninstall_script.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+. ~/blackjack_webapp/.build/virtualenv/bin/activate
 sudo systemctl stop nginx
 sudo pip3 uninstall -r requirements.txt -y
 sudo apt remove nginx -y
 sudo apt remove snapd -y
 sudo apt remove supervisor -y
-deactivate venv
+. ./deactivate
 sudo rm -r ~/blackjack_webapp
 echo "Uninstallation completed successfully"
